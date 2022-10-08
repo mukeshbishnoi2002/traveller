67773e16b5db97477cdf87725ae9223a672e6ab2
Mukesh <mukeshkumar42000000@gmail.com>
2022-10-08 15:29:52 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    <div>
     <div className="header">
     <div className="header-size">
-            <img src={tourtogo} alt="" />
             <h2>The Smart Traveller</h2>
     </div>
+</div>
 </div>
   );
 }
