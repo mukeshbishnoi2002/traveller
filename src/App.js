484d74e0cb84227877e5aedba5a6d4539fd1ca6f
Mukesh <mukeshkumar42000000@gmail.com>
2022-10-08 15:07:09 +0530

    aakash delete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>mukesh</h1>
-      <h2>aakash</h2>
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
